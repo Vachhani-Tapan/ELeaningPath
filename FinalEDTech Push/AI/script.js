f0ca1aaeec52db56e383e2777c7fc2cb646d181f
Vachhani-Tapan <tapan.vachhani.cg@gmail.com>
2025-12-31 18:54:44 +0530

    ELearning

--- FinalEDTech Push/AI/script.js
+++ FinalEDTech Push/AI/script.js
@@ -5,7 +5,7 @@ let imagebtn=document.querySelector("#image")
 let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
-const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyDS-4DkpWmxLSBBgkLz4v5StsChpSGQSso"
+const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyCmGdfsAIVwMumbWwiI9XYWsst6plZ2XlY"
 
 let user={
     message:null,
